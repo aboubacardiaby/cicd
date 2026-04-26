77c9203be12fccacc7e78990d320e0cf9b68da88
agassama1998 <melodygazzam1@gmail.com>
2026-04-26 14:27:02 -0500

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.addEventListener("scroll", function () {
+    document.querySelectorAll(".reveal").forEach(el => {
+        let top = el.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (top < windowHeight - 50) {
+            el.classList.add("active");
+        }
+    });
+});
